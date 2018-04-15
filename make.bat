489c6239d1ae6dc85de54f1da9cd7340f4251ea4
Jaime Soto <jsoto@simverge.com>
2018-04-15 19:58:39 -0400

    Simplify CMake build configuration and logging
    - Simplified CMake message after PDAL find_package call
    - Removed build type suffix from CMake build directory
    - Removed debug postfix variable argument from CMake project generation call

--- make.bat
+++ make.bat
@@ -7,17 +7,22 @@ if not defined GENERATOR set GENERATOR=Visual Studio 14 2015 Win64
 if not defined COMPILER_ID set COMPILER_ID=vc.14
 if not defined BUILD_TYPE set BUILD_TYPE=Release
 
-mkdir "%SCRIPT_DIR%\build\windows.amd64.%COMPILER_ID%.%BUILD_TYPE%"
-pushd "%SCRIPT_DIR%\build\windows.amd64.%COMPILER_ID%.%BUILD_TYPE%"
+set BUILD_DIR=%SCRIPT_DIR%\build\windows.amd64.%COMPILER_ID%
 
-mkdir install
+if exist "%BUILD_DIR%\pdal-c.sln" (
+	pushd "%BUILD_DIR%"
+) else (
+	mkdir "%BUILD_DIR%"
+	pushd "%BUILD_DIR%"
 
-cmake -G "%GENERATOR%" ^
-	-DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-	-DCMAKE_PREFIX_PATH=%OSGEO4W_ROOT% ^
-	-DCMAKE_DEBUG_POSTFIX=d ^
-	-DCMAKE_INSTALL_PREFIX=./install ^
-	../..
+	mkdir install
+
+	cmake -G "%GENERATOR%" ^
+		-DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+		-DCMAKE_PREFIX_PATH=%OSGEO4W_ROOT% ^
+		-DCMAKE_INSTALL_PREFIX=./install ^
+		../..
+)
 
 :: Build and install solution
 cmake --build . --target INSTALL --config %BUILD_TYPE%
