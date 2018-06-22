5cca1d57984e204a926f829f813c4706e126b741
Jaime Soto <jsoto@simverge.com>
2018-06-22 16:16:55 -0400

    Enable PointView projection string unit tests
    - Added new pipelines that enable projection string retrieval
    - Set GDAL_DATA and PROJ_LIB environment variables for Windows tests

--- source/pdal/capi/PointView.h
+++ source/pdal/capi/PointView.h
@@ -18,22 +18,78 @@ namespace pdal
 #include <stdbool.h> // for bool
 #include <stdint.h> // for uint64_t
 #endif
+			/**
+			 * Disposes the provided point `view`.
+			 * 
+			 * @param view The point view to dispose
+			 */
 			PDAL_C_API void PDALDisposePointView(PDALPointViewPtr view);
 
+			/**
+			 * Returns the ID of the provided point `view`.
+			 * 
+			 * @see pdal::PointView::id
+			 * 
+			 * @param view The point view
+			 * @return The view's ID
+			 */
 			PDAL_C_API int PDALGetPointViewId(PDALPointViewPtr view);
 
+			/**
+			 * Returns the number of points in the provided `view`.
+			 * 
+			 * @see pdal::PointView::size
+			 * 
+			 * @param view The point view
+			 * @return The number of points
+			 */
 			PDAL_C_API uint64_t PDALGetPointViewSize(PDALPointViewPtr view);
 
-			PDAL_C_API bool PDALIsPointViewEmpty(PDALPointViewPtr view);
 			/**
+			 * Returns whether the provided point `view` is empty, i.e., has no points.
+			 * 
+			 * @see pdal::PointView::empty
+			 * 
+			 * @param view The point view
+			 * @return Whether the point view is empty
+			 */
+			PDAL_C_API bool PDALIsPointViewEmpty(PDALPointViewPtr view);
+
+			/**
+			 * Clones the provided point `view`.
+			 * 
 			 * @see pdal::PointView::makeNew
+			 * 
+			 * @param view The original point view
+			 * @return A pointer to the new point view
 			 */
 			PDAL_C_API PDALPointViewPtr PDALClonePointView(PDALPointViewPtr view);
 
+			/**
+			 * Returns the proj4 projection string for the provided point `view`.
+			 *
+			 * @see pdal::PointView::spatialReference
+			 * 
+			 * @param view The point view
+			 * @param[out] proj The buffer used to hold the resulting proj4 projection string
+			 * @param size The size of the proj4 projection string buffer
+			 * @return The size of the resulting proj4 projection string
+			 */
 			PDAL_C_API size_t PDALGetPointViewProj4(PDALPointViewPtr view, char *proj, size_t size);
 
+			/**
+			 * Returns the Well-Known Text (WKT) projection string for the provided point `view`.
+			 * @see pdal::PointView::spatialReference
+			 * 
+			 * @param view The point view
+			 * @param[out] wkt The buffer used to hold the resulting WKT projection string
+			 * @param size The size of the wkt projection string buffer
+			 * @param pretty Whether to enhance the formatting of the resulting WKT string
+			 * @return The size of the resulting WKT projection string
+			 */
 			PDAL_C_API size_t PDALGetPointViewWkt(PDALPointViewPtr view, char *wkt, size_t size, bool pretty);
 
+			//! PDAL_C_API PDALPointLayoutPtr PDALGetPointViewLayout(PDALPointViewPtr view);
 
 			/**
 			 * Fill a buffer with point data specified by the dimension list.
@@ -42,10 +98,19 @@ namespace pdal
 			 * @param idx Index of point to get.
 			 * @param[out] buf Pointer to buffer to fill.
 			 */
+			//! PDAL_C_API void getPackedPoint(const DimTypeList& dims, PointId idx, char *buf);
 
+			//! PDAL_C_API void getPackedPoints(const DimTypeList& dims, PointId idx, char *buf);
 
 			/**
+			 * Fill a buffer with point data specified by the dimension list, accounts index
+			 * Using this functions it is possible to pack all points into one buffer
+			* @param pv    PointView pointer.
+			* @param dims  List of dimensions/types to retrieve.
+			* @param idx   Index of point to get.
+			* @param buf   Pointer to buffer to fill
 			*/
+			//! PDAL_C_API void appendPackedPoint(PointViewPtr pv, const DimTypeList& dims, PointId idx, std::size_t pointSize, char *buf)
 
 #ifdef __cplusplus
 		}
