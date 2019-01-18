4ff0ba68282d3c3ca65e7e64df514d456ae0c93b
Jaime Soto <jsoto@simverge.com>
2019-01-18 15:33:19 -0500

    Add barebones Travis CI build configuration
    Added a Travis CI configuration that runs a Release build and a Debug
    build in an Alpine-based Docker container with pdal-c build
    dependencies.

---
+++ tests/travis/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+gcc --version
+g++ --version
+
+set CI_PROJECT_DIR=/pdalc
+set CI_PROJECT_NAME=pdal-c
+
+export TARGET_PLATFORM=`uname`-`arch`-$BUILD_TYPE
+echo "Building $CI_PROJECT_NAME ($BRANCH branch) for $TARGET_PLATFORM"
+rm -rf "$CI_PROJECT_DIR/build/$TARGET_PLATFORM" 
+mkdir -p "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
+cd "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPDALC_GCC_PARAM_GGC_MIN_HEAPSIZE=8192 "$CI_PROJECT_DIR"
+make
+make coverage_pdalc
