6de4deff0a0731508fb3413c41b146bccc9a6a5b
Jaime Soto <jsoto@simverge.com>
2019-02-08 17:14:52 -0500

    Fix bash if spacing in travis config and script for ubuntu test

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -3,7 +3,7 @@
 gcc --version
 g++ --version
 
-if ["$DISTRO" = "ubuntu"]; then
+if [ "$DISTRO" = "ubuntu" ]; then
 	# Download and unzip the sonarcloud build wrapper
 	curl -LsS https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip > build-wrapper-linux-x86.zip
 	unzip build-wrapper-linux-x86.zip
@@ -19,7 +19,7 @@ mkdir -p "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
 cd "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPDALC_GCC_PARAM_GGC_MIN_HEAPSIZE=8192 "$CI_PROJECT_DIR"
 
-if ["$DISTRO" = "ubuntu"]; then
+if [ "$DISTRO" = "ubuntu" ]; then
 	$SONARCLOUD_DIR/build-wrapper-linux-x86-64 --out-dir $CI_PROJECT_DIR/bw-output make
 else
 	make
