ec076d2264e3c157b2328e3d6035e8c04562b5ff
Jaime Soto <jsoto@simverge.com>
2019-02-13 12:11:13 -0500

    Limit sonarcloud analysis to ubuntu release build in travis CI script

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -3,11 +3,13 @@
 gcc --version
 g++ --version
 
-if [ "$DISTRO" = "ubuntu" ]; then
+if [ "$DISTRO" = "ubuntu" && "$BUILD_TYPE" = "Release" ]; then
 	# Download and unzip the sonarcloud build wrapper
 	curl -LsS https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip > build-wrapper-linux-x86.zip
 	unzip build-wrapper-linux-x86.zip
-	export SONARCLOUD_DIR=$PWD/build-wrapper-linux-x86
+	rm build-wrapper-linux-x86.zip
+	export SONARCLOUD_DIR=${PWD}/build-wrapper-linux-x86
+
 fi
 
 export CI_PROJECT_DIR=/pdalc
@@ -19,8 +21,8 @@ mkdir -p "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
 cd "$CI_PROJECT_DIR/build/$TARGET_PLATFORM"
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPDALC_GCC_PARAM_GGC_MIN_HEAPSIZE=8192 "$CI_PROJECT_DIR"
 
-if [ "$DISTRO" = "ubuntu" ]; then
-	$SONARCLOUD_DIR/build-wrapper-linux-x86-64 --out-dir $CI_PROJECT_DIR/bw-output make
+if [ "$DISTRO" = "ubuntu" && "$BUILD_TYPE" = "Release" ]; then
+	${SONARCLOUD_DIR}/build-wrapper-linux-x86-64 --out-dir $CI_PROJECT_DIR/bw-output make
 else
 	make
 fi
