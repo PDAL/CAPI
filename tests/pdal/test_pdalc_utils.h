bde769ffe98e082562354236895c634ee8102639
Jaime Soto <420926+jimmysoda@users.noreply.github.com>
2019-05-09 16:23:06 -0400

    Add utility functions to read and load a PDAL pipeline
    Addresses the following Coverity Scan faults:
    
    **Argument cannot be negative  (CWE-687)**
    * [338166](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498516&defectInstanceId=10894463&mergedDefectId=338166)
    * [338167](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498519&defectInstanceId=10894464&mergedDefectId=338167)
    * [338168](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498520&defectInstanceId=10894466&mergedDefectId=338168)
    * [338169](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498517&defectInstanceId=10894465&mergedDefectId=338169)
    
    **Ignoring number of bytes read (CWE-252)**
    * [338170](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498520&defectInstanceId=10894460&mergedDefectId=338170)
    * [338171](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498517&defectInstanceId=10894462&mergedDefectId=338171)
    * [338172](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498516&defectInstanceId=10894459&mergedDefectId=338172)
    * [338173](https://scan4.coverity.com/reports.htm#v44582/p16422/fileInstanceId=60498519&defectInstanceId=10894461&mergedDefectId=338173)
    
    Also addresses previous SonarCloud code smell faults caused by duplicate code blocks.

---
+++ tests/pdal/test_pdalc_utils.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Copyright (c) 2019, Simverge Software LLC. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of Simverge Software LLC nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+#include <pdal/pdalc.h>
+
+/**
+ * Reads the contents of a PDAL pipeline file.
+ * 
+ * @note Caller obtains ownership of the returned string and is responsible for
+ *       freeing the allocated memory.
+ * 
+ * @param path The path to the PDAL pipeline JSON file
+ * @return The contents of the PDAL pipeline JSON file as a string
+ */
+const char *PDALReadPipelineJson(const char *path);
+
+/**
+ * Creates a PDAL pipeline from the provided path and attempts to execute it.
+ * 
+ * @note Caller obtains ownership of the returned pipeline and is responsible for
+ *       freeing the allocated memory with PDALDisposePipeline.
+ * @note The returned pipeline will be NULL if it could not be executed.
+ * 
+ * @param path The path to the PDAL pipeline JSON file
+ * @return The loaded PDAL pipeline
+ */
+PDALPipelinePtr *PDALLoadPipeline(const char *path);
